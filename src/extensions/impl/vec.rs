21ddd8e157d31cc4f86afcc45b2d32095203e0fe
Martin Ockajak <m@polytree.org>
2023-12-13 20:54:39 +0100

    .

--- src/extensions/impl/vec.rs
+++ src/extensions/impl/vec.rs
@@ -23,6 +23,33 @@ impl<A, B> ListMonad<A, B> for Vec<A> {
   }
 }
 
+impl<A> Iterable<A> for Vec<A> {
+  fn all(&self, predicate: impl Fn(&A) -> bool) -> bool {
+    self.iter().all(predicate)
+  }
+
+  fn any(&self, predicate: impl Fn(&A) -> bool) -> bool {
+    self.iter().any(predicate)
+  }
+
+  fn find(&self, predicate: impl Fn(&A) -> bool) -> Option<&A> where A: Clone {
+    self.iter().find(|&x| predicate(x))
+  }
+
+  fn fold<B>(&self, init: B, function: impl Fn(B, &A) -> B) -> B {
+    self.iter().fold(init, function)
+  }
+
+  fn reduce(&self, function: impl Fn(&A, &A) -> A) -> Option<A> where A: Clone {
+    let mut iterator = self.iter();
+    iterator.next().and_then(|init| Some(iterator.fold(init.clone(), |r, x| function(&r, x))))
+  }
+
+  fn rfold<B>(&self, init: B, function: impl Fn(B, &A) -> B) -> B {
+    self.iter().rfold(init, function)
+  }
+}
+
 impl<A: Clone> Collection<A> for Vec<A> {
   type C<X> = Vec<X>;
 
