e0f7b097a825e8ee9959b0801d851eb8e0edac1e
Martin Ockajak <m@polytree.org>
2023-12-13 01:07:03 +0100

    .

--- src/extensions/test.rs
+++ src/extensions/test.rs
@@ -6,8 +6,8 @@ mod tests {
   fn test_x() {
     let data = vec![1, 2, 3];
     let x = data.all(|x| x % 2 == 0);
-    let y = data.zip(&data);
-    let z = data.fold(0, |x| x + 1);
+    let y = data.fold(0, |r, x| r + x);
+    let z = data.zip(&data);
     assert_eq!(1, 1)
   }
 }
