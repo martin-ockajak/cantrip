c8bd1fc0f55dbe5182a3ca4a5247c2bf1464e16b
Martin Ockajak <m@polytree.org>
2023-12-13 19:02:36 +0100

    .

--- src/extensions/api/map.rs
+++ src/extensions/api/map.rs
@@ -5,3 +5,11 @@ pub trait Functor<K, V, L: Eq + Hash, W: Eq + Hash> {
 
   fn map(&self, function: impl Fn((&K, &V)) -> (L, W)) -> Self::C<L, W>;
 }
+
+pub trait Monad<K, V, L: Eq + Hash, W: Eq + Hash> {
+  type C<X, Y>;
+
+  fn unit(key: K, value: V) -> Self::C<K, V>;
+
+  fn flat_map(&self, function: impl Fn((&K, &V)) -> Self::C<L, W>) -> Self::C<L, W>;
+}
