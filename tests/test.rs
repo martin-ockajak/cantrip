2b90254b29a6a4fd998cb50c79e4bda7fdc402e4
Martin Ockajak <m@polytree.org>
2023-12-14 10:07:40 +0100

    .

--- tests/test.rs
+++ tests/test.rs
@@ -1,13 +1,10 @@
-#[cfg(test)]
-mod tests {
-  use cantrip::extensions::*;
+use cantrip::extensions::*;
 
-  #[test]
-  fn test_x() {
-    let data = vec![1, 2, 3];
-    data.all(|x| x % 2 == 0);
-    data.fold(0, |r, x| r + x);
-    data.clone().zip(&data);
-    assert_eq!(1, 1)
-  }
+#[test]
+fn test_x() {
+  let data = vec![1, 2, 3];
+  data.all(|x| x % 2 == 0);
+  data.fold(0, |r, x| r + x);
+  data.clone().zip(&data);
+  assert_eq!(1, 1)
 }
