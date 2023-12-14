814ff6435745fa9fac3043ca75df123245ebec34
Martin Ockajak <m@polytree.org>
2023-12-14 10:17:39 +0100

    .

--- tests/test.rs
+++ tests/test.rs
@@ -1,12 +1,13 @@
+use std::iter;
 use cantrip::extensions::*;
 
 #[test]
 fn test() {
   let data = vec![0, 1, 2];
-  data.map(|x| x + 1);               // Vec<i32>
-  data.flat_map(|x| unit(x + 1));    // Vec<i32>
-  data.fold(0, |r, x| r + x);        // i32
-  data.any(|x| x == 0);              // bool
-  data.clone().zip(&data);           // Vec<(i32, i32)>
+  data.map(|x| x + 1);                     // Vec<i32>
+  data.flat_map(|x| iter::once(x + 1));    // Vec<i32>
+  data.fold(0, |r, x| r + x);              // i32
+  data.any(|x| x == 0);                    // bool
+  data.clone().zip(&data);                 // Vec<(i32, i32)>
   assert_eq!(1, 1)
 }
