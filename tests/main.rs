60664e2d0a77d12fc157f5c0ae9fbbb8c3bdd0a1
Martin Ockajak <m@polytree.org>
2023-12-15 22:26:09 +0100

    .

--- tests/main.rs
+++ tests/main.rs
@@ -9,14 +9,14 @@ use crate::extensions::*;
 mod extensions;
 
 #[quickcheck]
+fn vec_str(data: Vec<String>) -> bool {
+  test_iterable(data.clone(), predicate_string) && test_ordered(data.clone(), predicate_string)
+}
+
 fn vec_i32(data: Vec<i32>) -> bool {
   test_iterable(data.clone(), predicate_i32)
     && test_ordered(data.clone(), predicate_i32)
     && test_aggregable(data.clone(), 0, |x, y| x.checked_add(y), 1, |x, y| x.checked_mul(y))
-}
-
-fn vec_str(data: Vec<String>) -> bool {
-  test_iterable(data.clone(), predicate_string) && test_ordered(data.clone(), predicate_string)
 }
 
 fn predicate_string(value: &String) -> bool {
