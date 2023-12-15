f4a3b16e2e7777fa4ac186ab9c466a91743b4da5
Martin Ockajak <m@polytree.org>
2023-12-15 22:25:47 +0100

    .

--- tests/extensions/mod.rs
+++ tests/extensions/mod.rs
@@ -1,4 +1,5 @@
 use cantrip::extensions::*;
+use std::iter::{Product, Sum};
 
 pub fn test_iterable<A, C>(data: C, mut predicate: impl FnMut(&A) -> bool) -> bool
 where
@@ -8,12 +9,44 @@ where
 }
 
 pub fn test_ordered<A, C>(data: C, mut predicate: impl FnMut(&A) -> bool) -> bool
-  where
-    C: Ordered<A> + IntoIterator<Item = A> + Clone,
+where
+  C: Ordered<A> + IntoIterator<Item = A> + Clone,
 {
   data.position(|x| predicate(x)) == data.clone().into_iter().position(|x| predicate(&x))
 }
-//
+
+pub fn test_aggregable<A, C>(
+  data: C, init_add: A, checked_add: impl FnMut(A, A) -> Option<A>, init_mul: A, checked_mul: impl FnMut(A, A) -> Option<A>,
+) -> bool
+where
+  A: PartialEq + Sum + Product,
+  C: Aggregable<A> + IntoIterator<Item = A> + Clone,
+{
+  (!safe_aggregate(data.clone(), init_add, checked_add) || data.clone().sum() == data.clone().into_iter().sum())
+    && (!safe_aggregate(data.clone(), init_mul, checked_mul) || data.clone().product() == data.clone().into_iter().product())
+}
+
+fn safe_aggregate<A, C>(data: C, init: A, mut aggregate: impl FnMut(A, A) -> Option<A>) -> bool
+where
+  A: PartialEq + Sum + Product,
+  C: Aggregable<A> + IntoIterator<Item = A> + Clone,
+{
+  let mut result = init;
+  let mut safe = true;
+  for value in data.into_iter() {
+    match aggregate(result, value) {
+      Some(number) => {
+        result = number;
+      }
+      None => {
+        safe = false;
+        break;
+      }
+    }
+  }
+  safe
+}
+
 // pub fn test_list<A, C>(data: C, mut predicate: impl FnMut(&A) -> bool) -> bool
 //   where
 //     C: List<A> + IntoIterator<Item = A> + Clone + PartialEq,
--- tests/main.rs
+++ tests/main.rs
@@ -10,7 +10,17 @@ mod extensions;
 
 #[quickcheck]
 fn vec_i32(data: Vec<i32>) -> bool {
-  test_iterable(data.clone(), predicate_i32) && test_ordered(data.clone(), predicate_i32)
+  test_iterable(data.clone(), predicate_i32)
+    && test_ordered(data.clone(), predicate_i32)
+    && test_aggregable(data.clone(), 0, |x, y| x.checked_add(y), 1, |x, y| x.checked_mul(y))
+}
+
+fn vec_str(data: Vec<String>) -> bool {
+  test_iterable(data.clone(), predicate_string) && test_ordered(data.clone(), predicate_string)
+}
+
+fn predicate_string(value: &String) -> bool {
+  value.len() % 2 == 0
 }
 
 fn predicate_i32(value: &i32) -> bool {
