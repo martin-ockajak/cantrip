23950b5f68040aa9ea52cf8f9c5c45b266c828de
Martin Ockajak <m@polytree.org>
2023-12-16 16:19:44 +0100

    .

--- tests/extensions/mod.rs
+++ tests/extensions/mod.rs
@@ -69,11 +69,12 @@ where
 pub fn test_list<A, C>(data: C) -> bool
 where
   A: TraversableFixture,
-  C: List<A> + IntoIterator<Item = A> + Clone,
+  C: List<A> + IntoIterator<Item = A> + FromIterator<A> + PartialEq + Clone,
   C::This<A>: PartialEq + FromIterator<A>,
 {
   let map = data.clone().map(|x| x.safe_add(x)) == data.clone().into_iter().map(|x| x.safe_add(&x)).collect();
-  map
+  let filter = data.clone().filter(|x| x.test()) == data.clone().into_iter().filter(|x| x.test()).collect();
+  map && filter
 }
 
 pub fn test_set<A, C>(data: C) -> bool
