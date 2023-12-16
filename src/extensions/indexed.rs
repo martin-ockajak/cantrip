08b1552143859dcd54bcc29984ab46724dc28ed4
Martin Ockajak <m@polytree.org>
2023-12-16 19:21:31 +0100

    .

--- src/extensions/indexed.rs
+++ src/extensions/indexed.rs
@@ -3,8 +3,10 @@ use std::collections::HashMap;
 use std::hash::Hash;
 use std::iter;
 use std::ops::RangeBounds;
+use std::rc::Rc;
 
 use crate::extensions::Iterable;
+use crate::extensions::util::unfold::unfold;
 
 pub trait Indexed<Item> {
   type This<I>;
@@ -22,24 +24,24 @@ pub trait Indexed<Item> {
     Self: IntoIterator<Item = Item>;
 
   // FIXME - make the moving work
-  // fn x_put(self, index: usize, element: A) -> Self
-  // where
-  //   Self: IntoIterator<Item = A> + Sized + FromIterator<A>,
-  // {
-  //   let mut iterator = self.into_iter();
-  //   let mut value = Rc::new(element);
-  //   unfold((0 as usize, false), |(current, done)| {
-  //     if !*done && *current == index {
-  //       *done = true;
-  //       None
-  //       // Rc::into_inner(value)
-  //     } else {
-  //       *current += 1;
-  //       iterator.next()
-  //     }
-  //   })
-  //   .collect()
-  // }
+  fn x_put(self, index: usize, element: Item) -> Self
+  where
+    Self: IntoIterator<Item = Item> + Sized + FromIterator<Item>,
+  {
+    let mut iterator = self.into_iter();
+    let mut value = Rc::new(element);
+    unfold((0 as usize, false), |(current, done)| {
+      if !*done && *current == index {
+        *done = true;
+        None
+        // Rc::into_inner(value)
+      } else {
+        *current += 1;
+        iterator.next()
+      }
+    })
+    .collect()
+  }
 
   fn replace(self, range: impl RangeBounds<usize>, replace_with: Self) -> Self
   where
