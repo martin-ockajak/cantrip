5df6f0021f110cbb29343a6d8b1ea183a5664daa
Martin Ockajak <m@polytree.org>
2023-12-21 20:39:45 +0100

    .

--- src/extensions/iterable.rs
+++ src/extensions/iterable.rs
@@ -226,13 +226,6 @@ pub trait Iterable<Item> {
   /// ```
   /// use cantrip::extensions::*;
   ///
-  /// assert_eq!(
-  ///     [2.4, f32::NAN, 1.3]
-  ///         .into_iter()
-  ///         .reduce(f32::max)
-  ///         .unwrap(),
-  ///     2.4
-  /// );
   /// ```
   ///
   /// # Examples
@@ -279,13 +272,6 @@ pub trait Iterable<Item> {
   /// ```
   /// use cantrip::extensions::*;
   ///
-  /// assert_eq!(
-  ///     [2.4, f32::NAN, 1.3]
-  ///         .into_iter()
-  ///         .reduce(f32::min)
-  ///         .unwrap(),
-  ///     1.3
-  /// );
   /// ```
   ///
   /// # Examples
