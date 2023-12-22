c7cc8f38ff0bf6b33f11fb0f1044b047f11f0c75
Martin Ockajak <m@polytree.org>
2023-12-22 14:36:11 +0100

    .

--- src/lib.rs
+++ src/lib.rs
@@ -15,23 +15,23 @@
 ///
 /// let data = vec![0, 1, 2];
 ///
-/// data.map(|x| x + 1);                  // [1, 2, 3]: Vec<i32>
+/// data.map(|x| x + 1);                  // vec![1, 2, 3]
 ///
-/// data.fold(0, |r, x| r + x);           // 3: i32
+/// data.fold(0, |r, x| r + x);           // 3
 ///
-/// data.any(|&x| x == 0);                // true: bool
+/// data.any(|&x| x == 0);                // true
 ///
-/// data.clone().filter(|&x| x > 0);      // [1, 2]: Vec<i32>
+/// data.clone().filter(|&x| x > 0);      // vec![1, 2]
 ///
-/// data.clone().add(0).distinct();       // [0, 1, 2]: Vec<i32>
+/// data.clone().add(0).distinct();       // vec![0, 1, 2]
 ///
-/// data.clone().delete(0).tail();        // [2]: Vec<i32>
+/// data.clone().delete(0).tail();        // vec![2]
 ///
-/// data.clone().grouped_by(|x| x % 2);   // {0: [0, 2], 1: [1]}: HashMap<i32, Vec<i32>>
+/// data.clone().grouped_by(|x| x % 2);   // HashMap::from(vec![(0, vec![0, 2]), (1, vec![1])])
 ///
-/// data.clone().partition(|&x| x > 1);   // ([2], [0, 1]): (Vec<i32>, Vec<i32>)
+/// data.clone().partition(|&x| x > 1);   // (vec![2], vec![0, 1])
 ///
-/// data.clone().zip(data);               // [(0, 0), (1, 1), (2, 2)]: Vec<(i32, i32)>
+/// data.clone().zip(data);               // vec![(0, 0), (1, 1), (2, 2)]
 /// ```
 ///
 /// ### Methods
