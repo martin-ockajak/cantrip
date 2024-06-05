59aa3d2e3bcb3f07f3058d4499f5de0b0c64497a
Martin Ockajak <m@polytree.org>
2024-06-05 15:40:26 +0200

    .

--- tests/extensions/collectible.rs
+++ tests/extensions/collectible.rs
@@ -8,36 +8,36 @@ pub fn test_collectible<'a, C>(sequence: bool)
 where
   C: Collectible<i64> + FromIterator<i64> + Iterable<Item<'a> = &'a i64> + Clone + PartialEq + Debug + 'a,
 {
-  let distinct = from::<C>(&[1, 2, 3]);
-  let repeated = from::<C>(&[1, 2, 2, 3]);
+  let distinct = from::<C>(&[0, 1, 2]);
+  let repeated = from::<C>(&[0, 1, 1, 2]);
   let empty = from::<C>(&[]);
 
   // add
-  assert_equal(distinct.clone().add(4), &[1, 2, 3, 4]);
+  assert_equal(distinct.clone().add(3), &[0, 1, 2, 3]);
   assert_equal(empty.clone().add(0), &[0]);
   if sequence {
-    assert_equal(distinct.clone().add(1), &[1, 2, 3, 1]);
+    assert_equal(distinct.clone().add(0), &[0, 1, 2, 0]);
   }
 
   // delete
-  assert_equal(distinct.clone().delete(&2), &[1, 3]);
-  assert_equal(distinct.clone().delete(&4), &[1, 2, 3]);
+  assert_equal(distinct.clone().delete(&1), &[0, 2]);
+  assert_equal(distinct.clone().delete(&3), &[0, 1, 2]);
   assert_equal(empty.clone().delete(&0), &[]);
   if sequence {
-    assert_equal(repeated.clone().delete(&2), &[1, 2, 3]);
+    assert_equal(repeated.clone().delete(&1), &[0, 1, 2]);
   }
 
   // delete_all
-  assert_equal(distinct.clone().delete_all(&vec![1, 2, 4]), &[3]);
-  assert_equal(distinct.clone().delete_all(&vec![]), &[1, 2, 3]);
+  assert_equal(distinct.clone().delete_all(&vec![0, 1]), &[2]);
+  assert_equal(distinct.clone().delete_all(&vec![]), &[0, 1, 2]);
   assert_equal(empty.clone().delete_all(&vec![0, 1]), &[]);
   if sequence {
-    assert_equal(repeated.clone().delete_all(&vec![1, 2, 4]), &[2, 3]);
+    assert_equal(repeated.clone().delete_all(&vec![0, 1, 3]), &[1, 2]);
   }
 
   // filter
-  assert_equal(distinct.clone().filter(|&x| x > 1), &[2, 3]);
-  assert_equal(distinct.clone().filter(|&x| x >= 0), &[1, 2, 3]);
+  assert_equal(distinct.clone().filter(|&x| x > 0), &[1, 2]);
+  assert_equal(distinct.clone().filter(|&x| x >= 0), &[0, 1, 2]);
   assert_equal(empty.clone().filter(|&x| x == 0), &[]);
 
   // filter_map
