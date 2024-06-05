08b91ea78a914bc060200fae43be0388d5b5e887
Martin Ockajak <m@polytree.org>
2024-06-05 05:26:41 +0200

    .

--- tests/extensions/collections.rs
+++ tests/extensions/collections.rs
@@ -7,7 +7,22 @@ use crate::extensions::reversible::test_reversible;
 use crate::extensions::traversable::test_traversable;
 use crate::extensions::sequence::test_sequence;
 
-pub fn test_vector<'a, C>()
+pub fn test_collectible_traits<'a, C>()
+  where
+    C: Traversable<i64>
+    + Collectible<i64>
+    + FromIterator<i64>
+    + Iterable<Item<'a> = &'a i64>
+    + Clone
+    + PartialEq
+    + Debug
+    + 'a,
+{
+  test_traversable::<C>();
+  // test_collectible::<C>();
+}
+
+pub fn test_sequence_traits<'a, C>()
 where
   C: Traversable<i64>
     + Collectible<i64>
