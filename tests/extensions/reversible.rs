cbeafdc0af7a027a9cd759581dcc05f993357e05
Martin Ockajak <m@polytree.org>
2024-06-05 15:42:37 +0200

    .

--- tests/extensions/reversible.rs
+++ tests/extensions/reversible.rs
@@ -8,9 +8,10 @@ pub fn test_reversible<'a, C>()
 where
   C: Reversible<i64> + FromIterator<i64> + Iterable<Item<'a> = &'a i64> + Clone + PartialEq + Debug + 'a,
 {
-  let values = from::<C>(&[1, 2, 3]);
-  // let empty = from::<C>(&[]);
+  let values = from::<C>(&[0, 1, 1, 2]);
+  let empty = from::<C>(&[]);
 
   // rposition
-  assert_eq!(values.rposition(|&x| x == 2), Some(1));
+  assert_eq!(values.rposition(|&x| x == 1), Some(2));
+  assert_eq!(empty.rposition(|&x| x == 0), None);
 }
--- tests/extensions/slice.rs
+++ tests/extensions/slice.rs
@@ -8,9 +8,10 @@ pub fn test_slice<'a, C>()
 where
   C: Slice<i64> + FromIterator<i64> + Iterable<Item<'a> = &'a i64> + Clone + PartialEq + Debug + 'a,
 {
-  let values = from::<C>(&[1, 2, 3]);
-  // let empty = from::<C>(&[]);
+  let values = from::<C>(&[0, 1, 2]);
+  let empty = from::<C>(&[]);
 
   // init
-  assert_eq!(values.init(), &from::<C>(&[1, 2]));
+  assert_eq!(values.init(), &from::<C>(&[0, 1]));
+  assert_eq!(empty.init(), &from::<C>(&[]));
 }
