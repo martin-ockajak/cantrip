8a8bc23014b125a9514494e8ba221703b2638340
Martin Ockajak <m@polytree.org>
2024-07-10 00:25:47 +0200

    .

--- src/extensions/iterable.rs
+++ src/extensions/iterable.rs
@@ -1,16 +1,83 @@
-#![allow(missing_docs)]
-
 use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, LinkedList, VecDeque};
 
+/// Conversion into an [`Iterator`] over references.
+///
+/// By implementing `Iterable` for a type, you define how it will be converted to
+/// an iterator over references. This is common for types which describe a
+/// collection of some kind.
+///
+/// One benefit of implementing `Iterable` is that your type will [work
+/// with Rust's `for` loop syntax](crate::iter#for-loops-and-intoiterator).
+///
+/// This is a reference-based equivalent of standard [`IntoIterator`] providing
+/// the functionality `iter()` methods for various standard collection types.
+///
+/// See also: [`FromIterator`].
+///
+/// [`IntoIterator`]: std::iter::IntoIterator
+///
+/// # Examples
+///
+/// Basic usage:
+///
+/// ```
+/// use crate::cantrip::*;
+///
+/// let v = vec![1, 2, 3];
+/// let mut iter = v.iterator();
+///
+/// assert_eq!(Some(&1), iter.next());
+/// assert_eq!(Some(&2), iter.next());
+/// assert_eq!(Some(&3), iter.next());
+/// assert_eq!(None, iter.next());
+/// ```
+/// Implementing `IntoIterator` for your type:
+///
+/// It is common to use `Iterable` as a trait bound. This allows the input collection type
+/// to change, so long as it is still an iterator. Additional bounds can be specified by
+/// restricting on `Item`:
+///
+/// ```
+/// use crate::cantrip::*;
+///
+/// fn collect_as_strings<'a, T>(collection: &'a impl Iterable<Item<'a> = &'a T>) -> Vec<String>
+/// where
+///   T: IntoIterator + 'a + std::fmt::Debug,
+/// {
+///   collection
+///     .iterator()
+///     .map(|item| format!("{item:?}"))
+///     .collect()
+/// }
+/// ```
 pub trait Iterable {
+  /// The type of the elements being iterated over.
   type Item<'collection>
   where
     Self: 'collection;
 
+  /// Which kind of iterator are we turning this into?
   type Iterator<'collection>: Iterator<Item = Self::Item<'collection>>
   where
     Self: 'collection;
 
+  /// Creates an iterator from a value.
+  ///
+  /// See the [`Iterable`] documentation for more.
+  ///
+  /// # Examples
+  ///
+  /// ```
+  /// use crate::cantrip::*;
+  ///
+  /// let v = vec![1, 2, 3];
+  /// let mut iter = v.iterator();
+  ///
+  /// assert_eq!(Some(&1), iter.next());
+  /// assert_eq!(Some(&2), iter.next());
+  /// assert_eq!(Some(&3), iter.next());
+  /// assert_eq!(None, iter.next());
+  /// ```
   #[allow(clippy::needless_lifetimes)]
   fn iterator<'c>(&'c self) -> Self::Iterator<'c>;
 }
