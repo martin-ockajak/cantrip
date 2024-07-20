88299b33dcc1ab85fdbfb98589c09f56597d2ae7
Martin Ockajak <m@polytree.org>
2024-07-20 15:52:07 +0200

    .

--- project/update-doc.sh
+++ project/update-doc.sh
@@ -10,6 +10,5 @@ echo >>README.md.tmp
 sed -n '/^pub.*/q;p' src/lib.rs | grep -v '# let' | grep -v '^#!' | sed 's/^\/\/! \?//' | sed 's/\[`/[/' | sed 's/`\](/](https:\/\/docs.rs\/cantrip\/latest\/cantrip\/trait./' | sed 's/::/.html#method./' | sed 's/\.html#method\.from/::from/' | sed 's/\.html#method\.\*/::*/' >>README.md.tmp
 echo -e "\n" >>README.md.tmp
 sed -n '/## Inspiration/,$ p' README.md >>README.md.tmp
-# mv -f README.md.tmp README.md
-cat README.md.tmp
+mv -f README.md.tmp README.md
 
