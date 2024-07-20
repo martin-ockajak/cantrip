55e1dc73c740a24d532eec518a93a47706c6e44a
Martin Ockajak <m@polytree.org>
2024-07-20 17:03:13 +0200

    .

--- project/update-doc.sh
+++ project/update-doc.sh
@@ -5,7 +5,7 @@ set -eo pipefail
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 cd ${SCRIPT_DIR}/..
 
-grep '^\[!\|^!\[' README.md >README.md.tmp
+grep '^\[!' README.md >README.md.tmp
 echo >>README.md.tmp
 sed -n '/^pub.*/q;p' src/lib.rs | grep -v '# let' | grep -v '^#!' | sed 's/^\/\/! \?//' | sed 's/\[`/[/' | sed 's/`\](/](https:\/\/docs.rs\/cantrip\/latest\/cantrip\/trait./' | sed 's/::/.html#method./' | sed 's/\.html#method\.from/::from/' | sed 's/\.html#method\.\*/::*/' >>README.md.tmp
 echo -e "\n" >>README.md.tmp
