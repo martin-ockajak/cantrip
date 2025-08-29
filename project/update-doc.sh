1ec717886f1b4905ed31104fcdd071cd56c41281
Martin Ockajak <m@polytree.org>
2025-08-29 21:51:40 +0200

    Update build

--- project/update-doc.sh
+++ project/update-doc.sh
@@ -5,7 +5,7 @@ set -eo pipefail
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 cd ${SCRIPT_DIR}/..
 
-sed -n '/^Practical/q;p' README.md >README.md.tmp
+sed -n '/^A Swiss/q;p' README.md >README.md.tmp
 sed -n '/^pub.*/q;p' src/lib.rs \
   | grep -v '^#!' \
   | grep -v '# let' \
